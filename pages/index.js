6b4b4b399896df48eadafe763bfaf2402645214f
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-05-15 08:21:46 +0530

    implimented loadNFTs to query unsold market items

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,51 @@
-import Head from "next/head";
-import Image from "next/image";
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Web3Modal from "web3modal";
+
+import { marketplaceAddress } from "../config";
+import NFTBazaar from "../artifacts/contracts/NFTBazaar.sol/NFTBazaar.json";
 
 export default function Home() {
+  const [nfts, setNFTs] = useState([]);
+  const [loadingState, setLoadingState] = useState("not-loaded");
+
+  useEffect(() => {
+    loadNFTs();
+  }, []);
+
+  const loadNFTs = async () => {
+    // how does this generic jsonRpcProvider work, hmmm?
+    const provider = new ethers.providers.JsonRpcProvider();
+    //the address needs to be changed
+    const contract = new ethers.Contract(
+      marketplaceAddress,
+      NFTBazaar.abi,
+      provider
+    );
+    const data = await contract.fetchMarketItems();
+
+    const items = await Promise.all(
+      data.map(async (i) => {
+        const tokenUri = await contract.tokenURI(i.tokenId);
+        const meta = await axios.get(tokenUri);
+        let price = ethers.utils.formatUnits(i.price.toString(), "ether");
+        let item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.data.image,
+          name: meta.data.name,
+          description: meta.data.description,
+        };
+        return item;
+      })
+    );
+
+    setNfts(items);
+    setLoadingState("loaded");
+  };
+
   return <>whoop</>;
 }
