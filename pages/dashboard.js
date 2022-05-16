cb330cb317e6bffe9df0152fcf7d2d701b95a045
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-05-16 09:12:22 +0530

    implimented loadNFTs calling fetchItemsListed()

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -9,5 +9,48 @@ import { marketplaceAddress } from "../config";
 import NFTBazaar from "../artifacts/contracts/NFTBazaar.sol/NFTBazaar.json";
 
 export default function CreatorDashboard() {
+  const [nfts, setNfts] = useState([]);
+  const [loadingState, setLoadingState] = useState("not-loaded");
+
+  useEffect(() => {
+    loadNFTs();
+  }, []);
+
+  async function loadNFTs() {
+    const web3Modal = new Web3Modal({
+      network: "mainnet",
+      cacheProvider: true,
+    });
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+
+    const contract = new ethers.Contract(
+      marketplaceAddress,
+      NFTBazaar.abi,
+      signer
+    );
+    const data = await contract.fetchItemsListed();
+
+    const items = await Promise.all(
+      data.map(async (i) => {
+        const tokenUri = await contract.tokenURI(i.tokenId);
+        const meta = await axios.get(tokenUri);
+        let price = ethers.utils.formatUnits(i.price.toString(), "ether");
+        let item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.data.image,
+        };
+        return item;
+      })
+    );
+
+    setNfts(items);
+    setLoadingState("loaded");
+  }
+
   return <>dashboard</>;
 }
