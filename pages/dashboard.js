e868c998c90c9146ef5b5f7a1f89188f45e8d8fa
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-05-16 09:08:06 +0530

    initialised creator dashboard

---
+++ pages/dashboard.js
@@ -0,0 +1,13 @@
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Web3Modal from "web3modal";
+import { useRouter } from "next/router";
+
+import { marketplaceAddress } from "../config";
+
+import NFTBazaar from "../artifacts/contracts/NFTBazaar.sol/NFTBazaar.json";
+
+export default function CreatorDashboard() {
+  return <>dashboard</>;
+}
