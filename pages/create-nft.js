481301df5aa131ead930c82d64a63f90660c1a1b
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-05-16 06:17:40 +0530

    implimented uploadToIPFS func

--- pages/create-nft.js
+++ pages/create-nft.js
@@ -5,3 +5,48 @@ import { useRouter } from "next/router";
 import Web3Modal from "web3modal";
 
 const client = ipfsHttpClient("https://ipfs.infura.io:5001/api/v0");
+
+import { marketplaceAddress } from "../config";
+
+import NFTBazaar from "../artifacts/contracts/NFTBazaar.sol/NFTBazaar.json";
+import { Input } from "postcss";
+
+export default function CreateItem() {
+  const [fileUrl, setFileUrl] = useState(null);
+  const [formInput, updateFormInput] = useState({
+    price: "",
+    name: "",
+    description: "",
+  });
+  const router = useRouter();
+
+  const onChange = async (e) => {
+    const file = e.target.files[0];
+    try {
+      const added = await client.add(file, {
+        progress: (progg) => console.log(`received: ${progg}`),
+      });
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+      setFileUrl(url);
+    } catch (error) {
+      console.log("Error uploading file: ", error);
+    }
+  };
+
+  async function uploadToIPFS() {
+    const { name, description, price } = formInput;
+    if (!name || !description || !price || !fileUrl) return;
+    const data = JSON.stringify({
+      name,
+      description,
+      image: fileUrl,
+    });
+    try {
+      const added = await client.add(data);
+      const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+      return url;
+    } catch (error) {
+      console.log("Error uploading file: ", error);
+    }
+  }
+}
