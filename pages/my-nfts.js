60b254df31edbc702b8ab9255eb3e4e9fcfe51f2
Abhishek Ranjan <rabhishekranjan@gmail.com>
2022-05-16 08:47:09 +0530

    loadNFTs fn implimented

--- pages/my-nfts.js
+++ pages/my-nfts.js
@@ -7,7 +7,7 @@ import { useRouter } from "next/router";
 
 import { marketplaceAddress } from "../config";
 
-import NFTMarketplace from "../artifacts/contracts/NFTMarketplace.sol/NFTMarketplace.json";
+import NFTBazaar from "../artifacts/contracts/NFTBazaar.sol/NFTBazaar.json";
 
 export default function MyAssets() {
   const [nfts, setNfts] = useState([]);
@@ -16,4 +16,47 @@ export default function MyAssets() {
   useEffect(() => {
     loadNFTs();
   }, []);
+
+  async function loadNFTs() {
+    const web3Modal = new Web3Modal({
+      network: "mainnet",
+      cacheProvider: true,
+    });
+    const connection = await web3Modal.connect();
+    const provider = new ethers.providers.Web3Provider(connection);
+    const signer = provider.getSigner();
+
+    const marketplaceContract = new ethers.Contract(
+      marketplaceAddress,
+      NFTBazaar.abi,
+      signer
+    );
+    const data = await marketplaceContract.fetchMyNFTs();
+
+    const items = await Promise.all(
+      data.map(async (i) => {
+        const tokenURI = await marketplaceContract.tokenURI(i.tokenId);
+        const meta = await axios.get(tokenURI);
+        let price = ethers.utils.formatUnits(i.price.toString(), "ether");
+        let item = {
+          price,
+          tokenId: i.tokenId.toNumber(),
+          seller: i.seller,
+          owner: i.owner,
+          image: meta.data.image,
+          tokenURI,
+        };
+        return item;
+      })
+    );
+    setNfts(items);
+    setLoadingState("loaded");
+  }
+  function listNFT(nft) {
+    console.log("nft:", nft);
+    router.push(`/resell-nft?id=${nft.tokenId}&tokenURI=${nft.tokenURI}`);
+  }
+  if (loadingState === "loaded" && !nfts.length)
+    return <h1 className="py-10 px-20 text-3xl">No NFTs owned</h1>;
+  return <>my-nfts</>;
 }
